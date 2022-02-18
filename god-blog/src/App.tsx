2a523787cc890125df5a687b858c6741d2d2df88
annan shao <43042815+annanShao@users.noreply.github.com>
2022-02-18 15:08:08 +0800

    feat: init react project

---
+++ god-blog/src/App.tsx
@@ -0,0 +1,34 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-18 12:54:40
+ * @LastEditTime: 2022-02-18 15:05:22
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \MyBlog\annanShao.github.io\god-blog\src\App.tsx
+ */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.2
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
